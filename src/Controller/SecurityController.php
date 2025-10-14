97a81c3441187a23a3931590df8d1d39437d4aa7
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-10-14 20:07:22 +0200

    Add login and logout methods to SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,32 @@
 
 namespace App\Controller;
 
-class SecurityController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastEmail = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_email' => $lastEmail,
+            'error' => $error
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Symfony gère la déconnexion (méthode vide intentionnellement)
+    }
 }
