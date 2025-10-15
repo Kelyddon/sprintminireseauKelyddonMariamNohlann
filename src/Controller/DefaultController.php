a8a60c575ead8635df6a3643aedfb109e68342d7
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-10-15 09:50:31 +0200

    redirection vers la page login si pas connecter

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,19 @@
 
 namespace App\Controller;
 
-class DefaultController
-{
+use App\Repository\MessageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(MessageRepository $messages): Response
+    {
+        $all = $messages->findBy([], ['id' => 'DESC']); // adapte si tu as createdAt
+        return $this->render('default/home.html.twig', [
+            'messages' => $all,
+        ]);
+    }
 }
