c6d5c31eb6bce135d67d2b42d6e9c744d6275484
Phelioume11 <nolhann.guillaume@outlook.com>
2025-10-15 12:12:37 +0200

    reunificationde la supression, la vue ciblé et l'ajout de messages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function home(MessageRepository $messages): Response
     {
-        $all = $messages->findBy([], ['id' => 'DESC']); // adapte si tu as createdAt
+        $all = $messages->findBy([], ['id' => 'DESC']); // tu peux changer 'id' par 'createdAt' si tu préfères
         return $this->render('default/home.html.twig', [
             'messages' => $all,
         ]);
+    }
+
+    #[Route('/message/{id}/delete', name: 'app_message_delete', methods: ['POST'])]
+    public function delete(Message $message, EntityManagerInterface $em): RedirectResponse
+    {
+        $user = $this->getUser();
+
+        // Vérifie si l'utilisateur est connecté et est l'auteur ou admin
+        if ($user && ($this->isGranted('ROLE_ADMIN') || $message->getAuthor() === $user)) {
+            $em->remove($message);
+            $em->flush();
+            $this->addFlash('success', 'Message supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Vous ne pouvez pas supprimer ce message.');
+        }
+
+        return $this->redirectToRoute('app_home');
     }
 }
--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Attribute\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MessageController extends AbstractController
 {
@@ -22,17 +22,8 @@ class MessageController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Associer l’auteur si la méthode/propriété existe (author ou user)
-            if (method_exists($message, 'setAuthor')) {
-                $message->setAuthor($this->getUser());
-            } elseif (method_exists($message, 'setUser')) {
-                $message->setUser($this->getUser());
-            }
-
-            // Date de création si supportée par l’entité
-            if (method_exists($message, 'getCreatedAt') && null === $message->getCreatedAt() && method_exists($message, 'setCreatedAt')) {
-                $message->setCreatedAt(new \DateTimeImmutable());
-            }
+            $message->setAuthor($this->getUser());
+            $message->setCreatedAt(new \DateTimeImmutable());
 
             $em->persist($message);
             $em->flush();
