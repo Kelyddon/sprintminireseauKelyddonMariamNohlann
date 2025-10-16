d4b414672a68b810e1fb9071feb087d17902ec50
Phelioume11 <nolhann.guillaume@outlook.com>
2025-10-16 14:53:06 +0200

    mailhog fonctionne

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -4,19 +4,26 @@ namespace App\Controller;
 
 use App\Entity\Message;
 use App\Form\MessageFormType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Attribute\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class MessageController extends AbstractController
 {
     #[Route('/message/add', name: 'app_message_add')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function add(Request $request, EntityManagerInterface $em): Response
-    {
+    public function add(
+        Request $request,
+        EntityManagerInterface $em,
+        MailerInterface $mailer,
+        UserRepository $userRepository
+    ): Response {
         $message = new Message();
         $form = $this->createForm(MessageFormType::class, $message);
         $form->handleRequest($request);
@@ -25,7 +32,7 @@ class MessageController extends AbstractController
             $message->setAuthor($this->getUser());
             $message->setCreatedAt(new \DateTimeImmutable());
 
-            // ✅ Gestion de l’image uploadée
+            // Gestion de l'image uploadée
             $imageFile = $form->get('image')->getData();
             if ($imageFile) {
                 $newFilename = uniqid('msg_') . '.' . $imageFile->guessExtension();
@@ -35,6 +42,20 @@ class MessageController extends AbstractController
 
             $em->persist($message);
             $em->flush();
+
+            // ✅ Envoi d'un mail à tous les utilisateurs
+            $users = $userRepository->findAll();
+            foreach ($users as $user) {
+                if ($user->getEmail()) {
+                    $email = (new Email())
+                        ->from('noreply@monsite.test')
+                        ->to($user->getEmail())
+                        ->subject('Nouveau message publié')
+                        ->text("Un nouveau message vient d'être publié par {$message->getAuthor()->getUsername()} :\n\n{$message->getContent()}");
+
+                    $mailer->send($email);
+                }
+            }
 
             $this->addFlash('success', 'Message publié avec succès.');
             return $this->redirectToRoute('app_home');
@@ -60,6 +81,7 @@ class MessageController extends AbstractController
     {
         $user = $this->getUser();
 
+        // Vérifie que seul l'auteur peut modifier
         if ($message->getAuthor() !== $user) {
             $this->addFlash('error', 'Vous ne pouvez modifier que vos propres messages.');
             return $this->redirectToRoute('app_home');
@@ -72,7 +94,7 @@ class MessageController extends AbstractController
             $imageFile = $form->get('image')->getData();
 
             if ($imageFile) {
-                // ✅ Supprime l’ancienne image si elle existe
+                // Supprime l'ancienne image si elle existe
                 if ($message->getImage()) {
                     $oldPath = $this->getParameter('uploads_dir') . '/' . $message->getImage();
                     if (file_exists($oldPath)) {
